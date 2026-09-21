e01c2f8910b91edb123a281573a98f7cfa01df59
chiwasushuba <joshua11084@gmail.com>
2026-09-22 03:08:35 +0800

    init backend spring boot

---
+++ src/main/java/com/aiops/aiops_backend/AiopsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aiops.aiops_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiopsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiopsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aiops/aiops_backend/AiopsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiops.aiops_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiopsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
